78f2d2a0d8e24720698c489380f27dfa8b8aa479
dingfan123 <dingfan1215@gmail.com>
2023-04-12 17:38:08 -0700

    DZ - add page for San Jose

---
+++ frontend/src/main/pages/SanJosePage.js
@@ -0,0 +1,16 @@
+import BasicLayout from "main/layouts/BasicLayout/BasicLayout";
+
+export default function SanJosePage() {
+  return (
+    <BasicLayout>
+      <div className="pt-2">
+        <h1>San Jose, CA</h1>
+        <p>
+         San Jose, California is a small town in Alameda County.  It is located about in Northern California.  It is known for its collection
+         of technology companies.
+        </p>
+        <p>Added by Dingfan Z.</p>
+      </div>
+    </BasicLayout>
+  )
+}
