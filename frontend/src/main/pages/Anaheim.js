e03e81b9c3a67a9ffbf4605a13764ee16e40a63c
Jason Vu <jasonvu@ucsb.edu>
2023-04-14 21:56:27 -0700

    edited pages

--- frontend/src/main/pages/Anaheim.js
+++ frontend/src/main/pages/Anaheim.js
@@ -6,10 +6,10 @@ export default function Anaheim() {
       <div className="pt-2">
         <h1>Anaheim, CA</h1>
         <p>
-         <b>>Anaheim, California</b> is a large town in Orange County.
+         <b>Anaheim, California</b> is a large town in Orange County.
          It is a popular tourist destination because it is home to the 
-	 Disneyland as well as the Anaheim Convention Center. It originally
-	 was an agricultural community until Disneyland opened in 1955.
+	       Disneyland as well as the Anaheim Convention Center. It originally
+	       was an agricultural community until Disneyland opened in 1955.
         </p>
         <p>Added by Jason V.</p>
       </div>
